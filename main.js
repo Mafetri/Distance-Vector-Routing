68448f2311dc258e0888cc2e94b8f5f619851049
Manuel Triñanes <manutritri2@gmail.com>
2023-12-25 15:24:33 -0300

    Send and Recv vector to/from neighbors interfaces

--- main.js
+++ main.js
@@ -41,7 +41,9 @@ let edges = [
 	},
 ];
 let tables = [];
+let vector_passes = [];
 
+// ======================== Initialization ========================
 // Graphs the network
 visual_interface.graph_network(nodes, edges);
 
@@ -88,21 +90,45 @@ function deep_copy(object) {
 	return copy;
 }
 
-// Send vector to neighbors
-function send_vector_to_neighbors(tables, nodes) {
-	nodes.forEach((node) => {
-		let table = tables.find((table) => table.table_node === node).table;
-		let neighbors = edges.filter((edge) => edge.nodes.includes(node));
-		neighbors.forEach((neighbor) => {
-			let neighborNode = neighbor.nodes.find((neighborNode) => neighborNode !== node);
-			let neighborTable = tables.find((table) => table.table_node === neighborNode).table;
-			let vector = table[node];
-			neighborTable[node] = deep_copy(vector);
-			visual_interface.update_vector(neighborNode, node, vector);
-		});
+// ======================== Interfaces Provided to Nodes ========================
+// Sends the vector to the neighbors of the node
+function broadcast_vector (vector, node) {
+	let neighbors = edges.filter((edge) => edge.nodes.includes(node));
+	neighbors.forEach((neighbor) => {
+		let neighbor_node = neighbor.nodes.find((neighbor_node) => neighbor_node !== node);
+		let packaged_vector = {
+			from: node,
+			to: neighbor_node,
+			vector: deep_copy(vector),
+		}
+		vector_passes.push(packaged_vector);
+		visual_interface.edge_send(node, neighbor_node);
 	});
 }
 
+// Receives the vectors sent to the node
+function recv_vector (node) {
+	let recv_vectors = vector_passes.filter(packaged_vector => packaged_vector.to === node);
+	vector_passes = vector_passes.filter(packaged_vector => packaged_vector.to !== node);
+	return recv_vectors;
+}
+
+// Updates the vectors of the node table
+function update_vectors (table, vectors, node) {
+	vectors.forEach((packaged_vector) => {
+		let neighbor_node = packaged_vector.from;
+		let neighbor_vector = packaged_vector.vector;
+		table[neighbor_node] = neighbor_vector;
+		visual_interface.update_vector(node, neighbor_node, neighbor_vector);
+	});
+}
+
+// Gives the table of the node
+function get_my_table (node) {
+	return tables.find((table) => table.table_node === node).table;
+}
+
+// Updates the node vector applying the Bellman-Ford equation
 function bellman_ford_equation(table, node) {
 	const targets_nodes = Object.keys(table[node]);
 	targets_nodes.forEach((target_node) => {
@@ -125,10 +151,22 @@ function bellman_ford_equation(table, node) {
 	});
 }
 
+// ======================== DV Algorithm ========================
 let flag = 0;
 document.getElementById("start_dv_algorithm").addEventListener("click", () => {
 	if (flag == 0) {
-		send_vector_to_neighbors(tables, nodes);
+		// Sends each vector
+		nodes.forEach((node) => {
+			// Broadcast vector to neighbors
+			broadcast_vector(get_my_table(node)[node], node);
+		});
+		// Receives each vector
+		nodes.forEach((node) => {
+			// Receive vectors from neighbors
+			let recv_vectors = recv_vector(node);
+			// Update vectors
+			update_vectors(get_my_table(node), recv_vectors, node);
+		});
 		document.getElementById("start_dv_algorithm").innerText = "Bellman";
 		flag = 1;
 	} else {
